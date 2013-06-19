f04ebcea0bafe2027a94facf61d2ea52c8e809bc
Justin Chan <hellojustinchan@gmail.com>
2013-06-19 10:17:05 -0700

    wth is going on

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,14 +6,11 @@ class HomeController < ApplicationController
 
 		if params[:name] != 'Lady Gaga'
 			if text["poker"] != nil
-				
 				url = URI.parse('https://api.groupme.com/v3/bots/post')
 				post_args = {"bot_id" => '87bd4bf2d3fad44c47c534ab36', "text" => "Pokerface"}.to_json
 				a = ActiveSupport::JSON.decode(post_args)
 				resp, data = Net::HTTP.post_form(url, a)
-
 			elsif text == 'kanye weather'
-
 				url = URI.parse('https://api.forecast.io/forecast/e9ae28050324270567556f2425a62c3f/37.6933,-121.9241')
 				resp_unparsed = Net::HTTP.get_response(url)
 				resp = JSON.parse resp_unparsed.body
@@ -25,7 +22,6 @@ class HomeController < ApplicationController
 				url = URI.parse('https://api.groupme.com/v3/bots/post')
 				a = ActiveSupport::JSON.decode(post_args)
 				resp, data = Net::HTTP.post_form(url, a)
-			
 			end
 		end
 	end
