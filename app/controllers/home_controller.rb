8f97dfafec122ddaf6f933443782622802948b64
Justin Chan <hellojustinchan@gmail.com>
2013-06-19 00:40:04 -0700

    derpy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,27 @@ class HomeController < ApplicationController
 	def index
 		require 'net/http'
 		require 'json'
-		url = URI.parse('https://api.groupme.com/v3/bots/post')
-		post_args = {"bot_id" => '87bd4bf2d3fad44c47c534ab36', "text" => "Poke whose face?"}.to_json
-		a = ActiveSupport::JSON.decode(post_args)
 		text = params[:text].downcase
+
 		if params[:name] != 'Lady Gaga'
 			if text["poker"] != nil
+				
+				url = URI.parse('https://api.groupme.com/v3/bots/post')
+				post_args = {"bot_id" => '87bd4bf2d3fad44c47c534ab36', "text" => "Pokerface"}.to_json
+				a = ActiveSupport::JSON.decode(post_args)
+				resp, data = Net::HTTP.post_form(url, a)
+
+			elsif if text == 'kanye weather'
+
+				url = URI.parse('https://api.forecast.io/forecast/e9ae28050324270567556f2425a62c3f/37.6933,-121.9241')
+				resp = Net::HTTP.get(url)
+				current_weather = resp[:currently]
+				temp = current_weather[:temperature]
+				summary = current_weather[:summary]
+				summary.downcase!
+				post_args = {"bot_id" => '87bd4bf2d3fad44c47c534ab36', "text" => "#{temp} degrees outside -- #{summary}."}.to_json
+				url = URI.parse('https://api.groupme.com/v3/bots/post')
+				a = ActiveSupport::JSON.decode(post_args)
 				resp, data = Net::HTTP.post_form(url, a)
 			end
 		end
