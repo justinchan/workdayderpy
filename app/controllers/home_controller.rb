fc59420e2276d83d8adf9c73a16f8f1cf755506c
Justin Chan <hellojustinchan@gmail.com>
2013-06-23 02:01:54 -0700

    derpy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -36,14 +36,31 @@ class HomeController < ApplicationController
 				resp = resp["response"]
 				count = resp["count"]
 				messages = resp["messages"]
-				temp_counter = 0
+				not_done_yet = true
+				running_count = 0
 				messages.each do |message|
-					temp_counter = temp_counter + 1
+					running_count = running_count + 1
+					message_id = message["id"]
+				end
+				while(not_done_yet)
+					url = URI.parse("https://api.groupme.com/groups/4767983/messages?token=ab32b920b6940130a343663e2468da7d&before_id=#{id}")
+					resp_unparsed = Net::HTTP.get_response(url)
+					resp = JSON.parse resp_unparsed.body
+					resp = resp["response"]
+					messages = resp["messages"]
+					messages.each do |message|
+						running_count = running_count + 1
+						message_id = message["id"]
+					end
+					if running_count == count
+						not_done_yet = false
+					end
 				end
 
 
+
 				url = URI.parse('https://api.groupme.com/v3/bots/post')
-				post_args = {"bot_id" => '87bd4bf2d3fad44c47c534ab36', "text" => "#{count} total messages. I counted #{temp_counter}."}.to_json
+				post_args = {"bot_id" => '87bd4bf2d3fad44c47c534ab36', "text" => "#{count} total messages. I counted #{running_count}."}.to_json
 				a = ActiveSupport::JSON.decode(post_args)
 				resp, data = Net::HTTP.post_form(url, a)
 			end
