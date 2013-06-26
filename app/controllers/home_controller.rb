0cd7903b7fb072af549d08f7d8c4da2badcd8e24
Justin Chan <hellojustinchan@gmail.com>
2013-06-26 15:14:22 -0700

    derpy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -122,15 +122,15 @@ class HomeController < ApplicationController
 				end
 				a = ActiveSupport::JSON.decode(post_args)
 				resp, data = Net::HTTP.post_form(url, a)
-			elsif text == 'kanye next train to SF'
+			elsif text == 'kanye next train to sf'
 				#insert stuff here
 				times = ""
-				# url = URI.parse("http://api.bart.gov/api/etd.aspx?cmd=etd&orig=wdub&key=MW9S-E7SL-26DU-VV8V&dir=s")
-				# resp_temp = Net::HTTP.get_response(url).body
-				# xml_data = REXML::Document.new(resp_temp)
-				# xml_data.elements.each('etd/estimate/minutes') do |time| 
-				# 	times << "#{time.text} " 
-				# end
+				url = URI.parse("http://api.bart.gov/api/etd.aspx?cmd=etd&orig=wdub&key=MW9S-E7SL-26DU-VV8V&dir=s")
+				resp_temp = Net::HTTP.get_response(url).body
+				xml_data = REXML::Document.new(resp_temp)
+				xml_data.elements.each('etd/estimate/minutes') do |time| 
+					times << "#{time.text} " 
+				end
 				url = URI.parse('https://api.groupme.com/v3/bots/post')
 				post_args = {"bot_id" => '87bd4bf2d3fad44c47c534ab36', "text" => "Trains leaving SF from West Dublin in #{times} minutes."}.to_json
 				a = ActiveSupport::JSON.decode(post_args)
