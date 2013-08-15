ea4ca00a9d97b877994a4457ec7b4bb31ed7dbcd
Justin Chan <hellojustinchan@gmail.com>
2013-08-15 02:10:07 -0700

    meep

--- app/controllers/fotos_controller.rb
+++ app/controllers/fotos_controller.rb
@@ -11,9 +11,7 @@ class FotosController < ApplicationController
 		messages = resp["messages"]
 
 		#Begin new shit
-		if resp["picture_url"]
-			@pictures << resp["picture_url"]
-		end
+		
 
 		not_done_yet = true
 		running_count = 0
@@ -21,6 +19,9 @@ class FotosController < ApplicationController
 		messages.each do |message|
 			running_count +=1
 			message_id = message["id"]
+			if message["picture_url"]
+				@pictures << resp["picture_url"]
+			end
 		end
 		while(not_done_yet)
 			url = URI.parse("https://api.groupme.com/groups/4600386/messages?token=ab32b920b6940130a343663e2468da7d&before_id=#{message_id}")
@@ -29,13 +30,13 @@ class FotosController < ApplicationController
 			resp = resp["response"]
 			messages = resp["messages"]
 
-			if resp["picture_url"]
-				@pictures << resp["picture_url"]
-			end
 
 			messages.each do |message|
 				running_count += 1
 				message_id = message["id"]
+				if message["picture_url"]
+					@pictures << resp["picture_url"]
+				end
 			end
 			if running_count == count
 				not_done_yet = false
