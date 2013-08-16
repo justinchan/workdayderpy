9e4bac76b2ef9a924382595e5435629c79fde41c
Justin Chan <hellojustinchan@gmail.com>
2013-08-15 19:21:36 -0700

    derpy

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,7 @@
 class Picture < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :name
-  has_attached_file :actual_picture, :storage => :dropbox, :dropbox_credentials => Rails.root.join("config/dropbox.yml"), :path => "/:username.:extension"
+  has_attached_file :actual_picture, :storage => :dropbox, :dropbox_credentials => Rails.root.join("config/dropbox.yml"), :path => "/:username.jpg"
 
   def picture_from_url(url_parm)
   	self.actual_picture = URI.parse(url_parm)
