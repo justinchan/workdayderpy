bbf53c323cc81b4e285ec1e9b81e0587f41a8292
Justin Chan <hellojustinchan@gmail.com>
2013-08-15 19:04:42 -0700

    derpy

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,8 +3,8 @@ class Picture < ActiveRecord::Base
   attr_accessible :url
   has_attached_file :actual_picture, :storage => :dropbox, :dropbox_credentials => Rails.root.join("config/dropbox.yml"), :path => "/:url.:extension"
 
-  def picture_from_url(url)
-  	self.actual_picture = URI.parse(url)
+  def picture_from_url(url_parm)
+  	self.actual_picture = URI.parse(url_parm)
   end
   
   Paperclip.interpolates :username do |attachment, style|
